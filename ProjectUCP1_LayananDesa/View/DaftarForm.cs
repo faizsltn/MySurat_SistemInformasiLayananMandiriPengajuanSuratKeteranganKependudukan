e4346dc835e8bcd2585c26c6733cc389da13e91e
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-05-18 04:54:56 +0700

    membuat koneksi dan input di tabel warga dab massage box

--- ProjectUCP1_LayananDesa/View/DaftarForm.cs
+++ ProjectUCP1_LayananDesa/View/DaftarForm.cs
@@ -24,3 +24,35 @@ namespace ProjectUCP1_LayananDesa
                 return;
             }
 
+            Koneksi kon = new Koneksi();
+            using (SqlConnection conn = kon.GetKoneksi())
+            {
+                try
+                {
+                    conn.Open();
+                    // Query untuk memasukkan data ke tabel WARGA
+                    string query = "INSERT INTO WARGA (nik, nama, password, nomor_hp, alamat) VALUES (@nik, @nama, @pass, @hp, @alamat)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@nik", txtNIK.Text);
+                    cmd.Parameters.AddWithValue("@nama", txtNama.Text);
+                    cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
+                    cmd.Parameters.AddWithValue("@hp", txtNoHP.Text);
+                    cmd.Parameters.AddWithValue("@alamat", txtAlamat.Text);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Akun berhasil dibuat! Silakan Login.", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Setelah sukses, balik ke Login
+                    LoginForm lg = new LoginForm();
+                    lg.Show();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal mendaftar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        //
