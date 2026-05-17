546c9334657f6ff1b0666186dc7c6c9a907618aa
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-05-18 04:55:53 +0700

    loads data

--- ProjectUCP1_LayananDesa/View/DaftarForm.cs
+++ ProjectUCP1_LayananDesa/View/DaftarForm.cs
@@ -63,3 +63,13 @@ namespace ProjectUCP1_LayananDesa
             lg.Show();
             this.Close();
         }
+
+        private void DaftarForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'layananDesa_DBDataSet.SURAT' table. You can move, or remove it, as needed.
+            this.sURATTableAdapter.Fill(this.layananDesa_DBDataSet.SURAT);
+
+        }
+    }
+}
+//
