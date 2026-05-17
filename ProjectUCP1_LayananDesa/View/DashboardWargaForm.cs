b64d9292fcf047e798de96e73bc8d6dbb3351cc6
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-05-18 04:48:19 +0700

    membuat class dasboard warga form, variable nik,

--- ProjectUCP1_LayananDesa/View/DashboardWargaForm.cs
+++ ProjectUCP1_LayananDesa/View/DashboardWargaForm.cs
@@ -1,3 +1,22 @@
 ﻿using System;
 using System.Windows.Forms;
 using ProjectUCP1_LayananDesa.View;//
+
+namespace ProjectUCP1_LayananDesa
+{
+    public partial class DashboardWargaForm : Form
+    {
+        // Kita siapkan variabel untuk menampung NIK asli agar bisa dipakai buat kirim surat
+        private string nikUser;
+
+        // SEKARANG: Constructor menerima DUA data (NIK dan Nama)
+        public DashboardWargaForm(string nik, string nama)
+        {
+            InitializeComponent();
+
+            // 1. Simpan NIK di memori (buat dipake di tombol pengajuan)
+            nikUser = nik;
+
+            // 2. Tampilkan NAMA di label (biar lebih ramah di layar)
+            lblNamaWarga.Text = "Selamat Datang, " + nama;
+        }
