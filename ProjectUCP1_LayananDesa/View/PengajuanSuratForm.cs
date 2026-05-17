55a951cef3cdf845324820f67124940147925a09
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-05-18 05:04:58 +0700

    membuat kelas pengajuan surat form

--- ProjectUCP1_LayananDesa/View/PengajuanSuratForm.cs
+++ ProjectUCP1_LayananDesa/View/PengajuanSuratForm.cs
@@ -3,3 +3,15 @@ using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using ProjectUCP1_LayananDesa.Controller;
+
+namespace ProjectUCP1_LayananDesa.View
+{
+    public partial class PengajuanSuratForm : Form
+    {
+        private string nikPengaju;
+
+        public PengajuanSuratForm(string nik)
+        {
+            InitializeComponent();
+            nikPengaju = nik;
+        }
