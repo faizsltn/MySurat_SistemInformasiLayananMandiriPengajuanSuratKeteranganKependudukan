311d0ee9d0cbc1afd2182bff6fb4830b7ec7a5b1
(faizsltn) <(faizsulthondaudm@gmail.com)>
2026-05-18 04:58:48 +0700

    membuat logikaa

--- ProjectUCP1_LayananDesa/View/DashboardAdminForm.cs
+++ ProjectUCP1_LayananDesa/View/DashboardAdminForm.cs
@@ -9,3 +9,24 @@ namespace ProjectUCP1_LayananDesa
     {
         // Variabel penampung identitas admin yang login
         private string idAdminAktif;
+
+        public DashboardAdminForm(string idAdmin, string namaAdmin)
+        {
+            InitializeComponent();
+            this.idAdminAktif = idAdmin;
+
+            // =========================================================================
+            // ANTISIPASI DOUBLE EVENT GANDA (Biar MessageBox tidak muncul 2x)
+            // =========================================================================
+            // Langkah 1: Cabut antrean click lama (jika sudah didaftarkan oleh desainer)
+            this.btnSetujui.Click -= this.btnSetujui_Click;
+            this.btnTolak.Click -= this.btnTolak_Click;
+            this.btnRefresh.Click -= this.btnRefresh_Click;
+            this.btnHapus.Click -= this.btnHapus_Click;
+
+            // Langkah 2: Daftarkan kembali secara bersih murni 1 jalur resmi
+            this.btnSetujui.Click += new System.EventHandler(this.btnSetujui_Click);
+            this.btnTolak.Click += new System.EventHandler(this.btnTolak_Click);
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+        }
